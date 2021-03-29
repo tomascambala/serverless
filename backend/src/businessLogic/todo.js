0d0c6185f0eaa0f786b3134c39db59c9bc742525
tomascambala <cambalatomas@gmail.com>
2021-03-29 17:56:33 +0200

    businessLogic

---
+++ backend/src/businessLogic/todo.js
@@ -0,0 +1,22 @@
+// nothing so far
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+
+export async function createTodo(userId: string, createTodoRequest: CreateTodoRequest): Promise<TodoItem> {
+    const todoId = uuid.v4()
+  
+    const newItem: TodoItem = {
+      userId,
+      todoId,
+      createdAt: new Date().toISOString(),
+      done: false,
+      attachmentUrl: null,
+      ...createTodoRequest
+    }
+  
+    logger.info(`Creating todo ${todoId} for user ${userId}`, { userId, todoId, todoItem: newItem })
+  
+    await todosAccess.createTodoItem(newItem)
+  
+    return newItem
+  }
+  
--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,5 +1,8 @@
 import 'source-map-support/register'
 
+import { createTodo } from '../../businessLogic/Todo'
+import { getUserId } from '../utils'
+
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
@@ -7,6 +10,17 @@ import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const userId = getUserId(event)
+  const newItem = await createTodo(userId, newTodo)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      item: newItem
+    })
+  }
 }
