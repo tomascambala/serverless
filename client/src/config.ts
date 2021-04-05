ff9c6b7fa263653a16274f4237546c049652b6ef
tomascambala <cambalatomas@gmail.com>
2021-04-05 18:35:03 +0200

    WIP

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-0pmdh13m.eu.auth0.com',      // Auth0 domain
+  clientId: 'HPQ3KRGGs8nAqWxQv6Ym7mzli7kjoxbX',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
